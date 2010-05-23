358f801f29401a96c57736641a45b3d257bff9a4
Tobias Bühlmann <tobias.buehlmann@gmx.de>
2010-05-23 21:43:11 +0200

    constantly using Pathname.new(...) instead of Pathname(...)

--- lib/ponder.rb
+++ lib/ponder.rb
@@ -3,11 +3,11 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup
 
-$LOAD_PATH.unshift Pathname(__FILE__).dirname.expand_path
+$LOAD_PATH.unshift Pathname.new(__FILE__).dirname.expand_path
 
 module Ponder
   def self.root
-    Pathname($0).dirname.expand_path
+    Pathname.new($0).dirname.expand_path
   end
   
   require 'ponder/version'
