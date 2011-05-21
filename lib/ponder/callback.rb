e845c22c226691c9ccb1f48d9187537b1b7881b5
Tobias Bühlmann <tobias.buehlmann@gmx.de>
2011-05-21 11:06:38 +0200

    removed whitespaces

--- lib/ponder/callback.rb
+++ lib/ponder/callback.rb
@@ -1,16 +1,16 @@
 module Ponder
   class Callback
     LISTENED_TYPES = [:connect, :channel, :query, :join, :part, :quit, :nickchange, :kick, :topic, :disconnect] # + 3-digit numbers
-    
+
     def initialize(event_type = :channel, match = //, proc = Proc.new {})
       unless self.class::LISTENED_TYPES.include?(event_type) || event_type.is_a?(Integer)
         raise TypeError, "#{event_type} is an unsupported event-type"
       end
-      
+
       self.match = match
       self.proc = proc
     end
-    
+
     def call(event_type, event_data = {})
       if (event_type == :channel) || (event_type == :query)
         @proc.call(event_data) if event_data[:message] =~ @match
@@ -20,9 +20,9 @@ module Ponder
         @proc.call(event_data)
       end
     end
-    
+
     private
-    
+
     def match=(match)
       if match.is_a?(Regexp)
         @match = match
@@ -30,7 +30,7 @@ module Ponder
         raise TypeError, "#{match} must be a Regexp"
       end
     end
-    
+
     def proc=(proc)
       if proc.is_a?(Proc)
         @proc = proc
