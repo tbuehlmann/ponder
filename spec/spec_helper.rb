e239ffa6641d9bd4a7cdadc676fb931cef383a64
tbuehlmann <tobias.buehlmann@gmx.de>
2013-02-01 13:03:10 +0100

    Using colored RSpec output. [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rspec'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+end
